087568ed35d5a9aae79cc035b83e00ceb2752e53
Gustavo-Arj <149444842+Gustavo-Arj@users.noreply.github.com>
2024-10-23 07:49:07 +0000

    features: completed task

--- src/routes/private/PrivateRoutes.js
+++ src/routes/private/PrivateRoutes.js
@@ -3,6 +3,8 @@ import React from 'react'
 import TabNavigator from './TabNavigator';
 import UserProfileScreen from '../../screens/UserProfileScreen';
 import PostScreen from '../../screens/PostScreen';
+import Comments from '../../screens/Comments';
+import SearchPosts from '../../screens/SearchPosts';
 
 
 const Stack = createStackNavigator();
@@ -15,9 +17,10 @@ export default function PrivateRoutes() {
             }} component={TabNavigator} />
             <Stack.Screen name="UserProfileScreen" component={UserProfileScreen} />
             <Stack.Screen name="PostScreen" component={PostScreen} />
-
-
+            <Stack.Screen name="SearchPosts" component={SearchPosts} />
             
+            <Stack.Screen name="Comments" component={Comments} />
+
         </Stack.Navigator>
     )
 }
